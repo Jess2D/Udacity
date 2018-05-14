82222d1c6dd9462db1feb1ca2cfeece043e3957e
Jess <jessicap_silveira@hotmail.com>
2018-05-13 21:07:54 -0300

    PROJETO PIXEL ART

---
+++ project-pixel-art-maker/designs.js
@@ -0,0 +1,22 @@
+$('#sizePicker').submit(function(event){
+    event.preventDefault(); 
+    let height = $('#inputHeight').val(); // valor das linhas
+    let weight = $('#inputWeight').val(); //valor das colunas
+    makeGrid(height, weight); //ao clicar submit chama função makeGrid()
+});
+
+function makeGrid(x,y) {
+    $('table tr').remove(); 
+    for(let i = 1; i<=x ; i++){
+        $('#pixelCanvas').append(`<tr id = linha${i}></tr>`); //cria as linhas
+        for(let j = 1; j<=y; j++){
+            $(`#linha${i}`).append("<td></td>"); //preenche as linhas
+        }
+    }
+
+    $('td').click(function(){
+      let color = $('#colorPicker').val(); // valor da cor 
+      $(event.target).css('background-color', color);
+    });
+
+}
